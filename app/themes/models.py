8fe8723783dfd385929cc6ead5a088ddfc562e8e
Vladius25 <vkorol2509@icloud.com>
2020-08-02 16:28:11 +0500

    Add relation with Category

--- app/themes/models.py
+++ app/themes/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from filebrowser.fields import FileBrowseField
 
+from categories.models import Category
 from levels.models import Level
 from words.models import Word
 
 
 class Theme(models.Model):
     name = models.CharField("Название темы", max_length=100)
-    level = models.ForeignKey(Level, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+    level = models.ForeignKey(Level, on_delete=models.SET_NULL, verbose_name="Уровень", null=True)
     photo = FileBrowseField(
         "Иконка темы",
         max_length=200,
@@ -16,7 +18,7 @@ class Theme(models.Model):
         extensions=[".jpg", ".jpeg", ".gif", ".png", ".JPG", ".PNG", ".JPEG", ".GIF"],
         null=True
     )
-    words = models.ManyToManyField(Word)
+    words = models.ManyToManyField(Word, verbose_name="Слова", blank=True)
 
     class Meta:
         verbose_name = "Тема"
