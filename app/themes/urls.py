40a0f7d1ba2b1e08242bdb35d86e4e260c6238e7
Vladius25 <vkorol2509@icloud.com>
2020-08-02 16:45:19 +0500

    Add new controller to themes

--- app/themes/urls.py
+++ app/themes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from themes.views import GetThemeView
+from themes.views import GetThemeView, GetThemesView
 
 urlpatterns = [
+    path("", GetThemesView.as_view(), name="get-themes"),
     path("<int:pk>/", GetThemeView.as_view(), name="get-theme")
 ]
--- app/themes/views.py
+++ app/themes/views.py
@@ -9,3 +9,15 @@ class GetThemeView(generics.RetrieveAPIView):
     queryset = Theme.objects.all()
     serializer_class = ThemeSerializerWithWords
 
+
+class GetThemesView(generics.ListAPIView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = ThemeSerializer
+
+    def get_queryset(self):
+        queryset = Theme.objects.all()
+        for param_name in ("category", "level"):
+            param = self.request.query_params.get(param_name, None)
+            if param:
+                queryset = queryset.filter(**{param_name: param})
+        return queryset
