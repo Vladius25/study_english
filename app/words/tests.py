a07e8f54816b52209752b8113d004420beef8e0a
Vladius25 <vkorol2509@icloud.com>
2020-08-02 17:27:20 +0500

    Add test to words

--- app/words/tests.py
+++ app/words/tests.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
 from words.models import Word
+from words.serializers import WordSerializerShort
 
 
 class TestWords(APITestCase):
@@ -16,8 +17,8 @@ class TestWords(APITestCase):
             "example": "John has asked Mary out several times.",
             "sound": "uploads/toaskout.mp3",
         }
-        Word.objects.create(**cls.word)
-        Word.objects.create(**cls.word)
+        cls.word1 = Word.objects.create(**cls.word)
+        cls.word2 = Word.objects.create(**cls.word)
 
     def test_return_200(self):
         response = self.client.get("/api/v1/words/1/")
@@ -29,6 +30,11 @@ class TestWords(APITestCase):
         del self.word["sound"]
         del response.data["sound"]
         self.assertDictEqual(response.data, self.word)
+
+    def test_short_serialiser(self):
+        serializer = WordSerializerShort()
+        data = serializer.to_representation(self.word1)
+        self.assertDictEqual(data, {"id": 1, "name": self.word["name"]})
 
     def test_sound_url_absolute(self):
         response = self.client.get("/api/v1/words/1/", format="json")
